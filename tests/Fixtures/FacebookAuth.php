5e52b9bde0866a9ba2aeb1153a1f84e128764f34
Milan Felix Šulc <rkfelix@gmail.com>
2022-07-18 10:13:53 +0200

    Tests: fixes

---
+++ tests/Fixtures/FacebookAuth.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Fixtures;
+
+class FacebookAuth
+{
+
+	/**
+	 * @return string
+	 */
+	public function getFacebookAppId()
+	{
+		return 'appId';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFacebookSecret()
+	{
+		return 'secret';
+	}
+
+}
